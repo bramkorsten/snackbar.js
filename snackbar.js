2699b21efbc9a46b8292864498d9a41baabd9955
bram <bram@bramkorsten.io>
2016-11-15 08:30:06 -0500

    Updated to better follow the material design guidelines. Updated animations.

--- snackbar.js
+++ snackbar.js
@@ -7,9 +7,16 @@
  * conditions of the MIT license, available at http://www.opensource.org/licenses/mit-license.php
  *
  */
+
+$.easing.material = function (x, t, b, c, d) {
+        if ((t/=d/2) < 1) return c/2*t*t*t + b;
+        return c/2*((t-=2)*t*t + 2) + b;
+}
+
 function snackbar() {
   
   var type = 'message';
+  var active = false;
   
   this.success = function(message) {
     this.type = "message";
@@ -22,35 +29,58 @@ function snackbar() {
   };
   
   function createContainer(message, type) {
-    var snackbarcontainer = document.createElement("div");
-    snackbarcontainer.className = "snackbar-wrapper";
-    snackbarcontainer.innerHTML = '<p>' + message + '</p><a>okay</a>';
-    document.body.appendChild(snackbarcontainer);
-    $( ".snackbar-wrapper a" ).click(function() {
-            $('.snackbar-wrapper').stop().animate(
+    var timeout;
+    if (active) {
+      clearTimeout(timeout);
+    $('.snackbar-wrapper').stop().animate(
                     {
                         bottom: '-50px'
                     },
                     500,
                     function() {
                         $('.snackbar-wrapper').remove();
+                      newContainer();
                     }
                 );
-    });
-    $(".snackbar-wrapper").addClass(type);
-    $(".snackbar-wrapper").stop().animate({ bottom:'0' }, 500);
-    setTimeout(function(){
-      $('.snackbar-wrapper').stop().animate(
-                    {
-                        bottom: '-50px'
-                    },
-                    500,
-                    function() {
-                        $('.snackbar-wrapper').remove();
-                    }
-                );
-    }, 5000);
-
+      active = false;
+    }
+    else {
+      newContainer();
+    }
+    function newContainer() {
+      var snackbarcontainer = document.createElement("div");
+      snackbarcontainer.className = "snackbar-wrapper";
+      snackbarcontainer.innerHTML = '<p>' + message + '</p><a>okay</a>';
+      document.body.appendChild(snackbarcontainer);
+      active = true;
+      $( ".snackbar-wrapper a" ).click(function() {
+              $('.snackbar-wrapper').stop().animate(
+                      {
+                          bottom: '-50px'
+                      },
+                      500, "material",
+                      function() {
+                          $('.snackbar-wrapper').remove();
+                          active = false;
+                      }
+                  );
+      });
+      $(".snackbar-wrapper").addClass(type);
+      $(".snackbar-wrapper").stop().animate({ bottom:'0' }, 500, "material");
+      $(".snackbar-wrapper p").stop().animate({ opacity:'1' }, 500, "material");
+      timeout = setTimeout(function(){
+        $('.snackbar-wrapper').stop().animate(
+                      {
+                          bottom: '-50px'
+                      },
+                      500,
+                      function() {
+                          $('.snackbar-wrapper').remove();
+                          active = false;
+                      }
+                  );
+      }, 5000);
+    }
   }
 }
 
